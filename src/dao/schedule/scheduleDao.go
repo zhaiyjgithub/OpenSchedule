26ce07ecba9b966d208b18b8a9926e135afba5ae
zhaiyuanji <chuck.zhaiyj@gmail.com>
2021-10-08 16:38:31 +0800

    add calc next available date time.

--- src/dao/schedule/scheduleDao.go
+++ src/dao/schedule/scheduleDao.go
@@ -2,7 +2,12 @@ package schedule
 
 import (
 	"OpenSchedule/src/model/doctor"
+	"OpenSchedule/src/utils"
+	"errors"
 	"gorm.io/gorm"
+	"strconv"
+	"strings"
+	"time"
 )
 
 type Dao struct {
@@ -34,4 +39,72 @@ func (d *Dao) GetScheduleSettings(npi int64) *doctor.ScheduleSettings {
 		return nil
 	}
 	return st
+}
+
+func (d *Dao) SyncScheduleNextAvailableDateToES()  {
+
+}
+
+func (d *Dao) CalcNextAvailableDate(t time.Time, settings *doctor.ScheduleSettings) (bool, string)  {
+	weekDay := t.Weekday()
+	duration := settings.DurationPerSlot
+	number := settings.NumberPerSlot
+	isOk := false
+	nextAvailableDate := ""
+	if weekDay == time.Sunday {
+		isOk, nextAvailableDate = d.CalcNextAvailableDateForEachWeekDay(t, settings.SundayAmStartTime, settings.SundayAmEndTime,
+			settings.SundayPmStartTime, settings.SundayPmEndTime,duration, number)
+	}else if weekDay == time.Friday {
+		isOk, nextAvailableDate = d.CalcNextAvailableDateForEachWeekDay(t, settings.FridayAmStartTime, settings.FridayAmEndTime,
+			settings.FridayPmStartTime, settings.FridayPmEndTime,duration, number)
+	}
+	return isOk, nextAvailableDate
+}
+
+func (d *Dao) CalcNextAvailableDateForEachWeekDay(t time.Time,
+	weekDayAMStartTime string, weekDayAMEndTime string, weekDayPMStartTime string, weekDayPMEndTime string,
+	durationOfSlot int, numberOfSlot int) (bool, string) {
+	AMStartTime, err := d.ParseScheduleTimeToUTC(weekDayAMStartTime)
+	AMEndTime, err := d.ParseScheduleTimeToUTC(weekDayAMEndTime)
+
+	PMStartTime, err := d.ParseScheduleTimeToUTC(weekDayPMStartTime)
+	PMEndTime, err := d.ParseScheduleTimeToUTC(weekDayPMEndTime)
+	if err != nil {
+		return false, ""
+	}
+	if t.Before(AMStartTime)  {
+		return true, time.Now().UTC().Format(time.RFC3339)
+	}else if t.After(AMStartTime) && t.Before(AMEndTime) {
+		nextAvailableDateTime := d.CalcNextAvailableDateForTimeRange(t, AMStartTime, durationOfSlot)
+		return true, nextAvailableDateTime
+	}else if t.After(PMStartTime) && t.Before(PMEndTime) {
+		nextAvailableDateTime := d.CalcNextAvailableDateForTimeRange(t, PMStartTime, durationOfSlot)
+		return true, nextAvailableDateTime
+	}
+	return false, ""
+}
+
+func (d *Dao) CalcNextAvailableDateForTimeRange(t time.Time, startTime time.Time, durationOfSlot int) string {
+	availableTimeRangeMinutes := t.Sub(startTime).Minutes()
+	slotTimeNumber := int(availableTimeRangeMinutes) / durationOfSlot
+	slotTime := slotTimeNumber*durationOfSlot
+	nextAvailableDateTime := startTime.Add(time.Duration(slotTime)*time.Minute).Format(time.RFC3339)
+	return nextAvailableDateTime
+}
+
+func (d *Dao)ParseScheduleTimeToUTC(scheduleTime string) (time.Time, error) {
+	if !utils.CheckDateTime(scheduleTime) {
+		return time.Now().UTC(), errors.New("param error")
+	}
+
+	t := time.Now().UTC()
+	year := t.Year()
+	month := t.Month()
+	day := t.Day()
+
+	dateTime := strings.Split(scheduleTime, ":")
+	hour, _ := strconv.Atoi(dateTime[0])
+	min, _ := strconv.Atoi(dateTime[1])
+	utcTime := time.Date(year, month, day, hour, min, 0, 0, time.UTC)
+	return utcTime, nil
 }
--- src/dao/schedule/scheduleDao_test.go
+++ src/dao/schedule/scheduleDao_test.go
@@ -3,7 +3,9 @@ package schedule
 import (
 	"OpenSchedule/src/database"
 	"OpenSchedule/src/model/doctor"
+	"fmt"
 	"testing"
+	"time"
 )
 
 func TestDao_SetScheduleSettings(t *testing.T) {
@@ -17,3 +19,13 @@ func TestDao_SetScheduleSettings(t *testing.T) {
 		t.Errorf("test failed")
 	}
 }
+
+func TestDao_ParseScheduleTimeToUTC(t *testing.T) {
+	dao := NewDao(database.GetMySqlEngine())
+	stime, err := dao.ParseScheduleTimeToUTC("09:45")
+	if err != nil {
+		t.Errorf("test failed")
+	}
+	fmt.Println(stime.Format(time.RFC3339))
+}
+
