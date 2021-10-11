759ffbfd76a10805b1c1a4cb960f0d5c0d76a7f6
zhaiyuanji <chuck.zhaiyj@gmail.com>
2021-10-11 22:10:38 +0800

    add service api for sync settings.

--- src/service/schedule/scheduleService.go
+++ src/service/schedule/scheduleService.go
@@ -1,10 +1,12 @@
 package schedule
 
 import (
+	"OpenSchedule/src/constant"
 	"OpenSchedule/src/dao/schedule"
 	"OpenSchedule/src/database"
 	"OpenSchedule/src/model/doctor"
-	"github.com/olivere/elastic/v7"
+	"errors"
+	"time"
 )
 
 type Service interface {
@@ -13,24 +15,39 @@ type Service interface {
 }
 
 func NewService() Service {
-	return &service{dao: schedule.NewDao(database.GetMySqlEngine())}
+	return &service{dao: schedule.NewDao(database.GetMySqlEngine(), database.GetElasticSearchEngine())}
 }
 
 type service struct {
 	dao *schedule.Dao
-	esEngine *elastic.Client
 }
 
 func (s *service) SetScheduleSettings(setting *doctor.ScheduleSettings) error {
-	return s.dao.SetScheduleSettings(setting)
+	err := s.dao.SetScheduleSettings(setting)
+	if err != nil {
+		return err
+	}
 	//begin to sync the certain doctor next available date.
+	err = s.SyncCertainDoctorScheduleNextAvailableDateToES(setting)
+	return err
 }
 
 func (s *service) GetScheduleSettings(npi int64) *doctor.ScheduleSettings {
 	return s.dao.GetScheduleSettings(npi)
 }
 
-func (s *service) SyncCertainDoctorScheduleNextAvailableDateToES(setting *doctor.ScheduleSettings)  {
+func (s *service) SyncCertainDoctorScheduleNextAvailableDateToES(setting *doctor.ScheduleSettings) error  {
 	//get the next available date
 	//begin to update the es for certain doctor
+	currentTime := time.Now().UTC()
+	isOk, nextAvailableDateInClinic := s.dao.CalcNextAvailableDate(currentTime, constant.InClinic, setting)
+	if !isOk {
+		return errors.New("calc nextAvailable Date in clinic failed")
+	}
+	isOk, nextAvailableDateVirtual := s.dao.CalcNextAvailableDate(currentTime, constant.InClinic, setting)
+	if !isOk {
+		return errors.New("calc nextAvailable date virtual failed")
+	}
+	err := s.dao.SyncCertainDoctorNextAvailableDateToES(setting.Npi, nextAvailableDateInClinic, nextAvailableDateVirtual)
+	return err
 }
